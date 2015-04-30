e4188d984ded019d113efb6efc0b3419731e8c19
pau.ferrer-ocana <crazyserver@gmail.com>
2015-04-30 11:30:09 +0200

    Local agora needs Moodle 2.8 to work

--- version.php
+++ version.php
@@ -3,4 +3,4 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version  = 2015050400;   // The (date) version of this plugin
-$plugin->requires = 2013111806;   // Requires this Moodle version
+$plugin->requires = 2014111005;   // Requires this Moodle version
