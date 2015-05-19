eeda8322f099b2088f30ae0710f34fe5f6cac795
crazyserver <crazyserver@gmail.com>
2015-05-19 14:51:10 +0200

    Disable some unuseful blocks

--- version.php
+++ version.php
@@ -2,5 +2,5 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2015050400;   // The (date) version of this plugin
+$plugin->version  = 2015051900;   // The (date) version of this plugin
 $plugin->requires = 2014111005;   // Requires this Moodle version
