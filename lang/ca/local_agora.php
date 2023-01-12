1253611bc31e884469a76a11cc7da14a46f5398b
Toni Ginard <toni.ginard@gmail.com>
2023-01-12 16:04:12 +0100

    Removed unused language strings

--- lang/ca/local_agora.php
+++ lang/ca/local_agora.php
@@ -13,38 +13,10 @@ $string['capitalize'] = 'Posa la primera lletra en majúscules';
 $string['capitalizecheckfull'] = 'Estàs segur/a que vols posar la primera lletra en majúscules als usuaris {$a} ?';
 $string['capitalizenot'] = 'No ha estat possible posar la primera lletra en majúscules als usuaris {$a} !';
 
-//Message notifications
-$string['now'] = 'ara';
-$string['second_ago'] = 'fa {$a} segon';
-$string['seconds_ago'] = 'fa {$a} segons';
-$string['minute_ago'] = 'fa {$a} minut';
-$string['minutes_ago'] = 'fa {$a} minuts';
-$string['hour_ago'] = 'fa {$a} hora';
-$string['hours_ago'] = 'fa {$a} hores';
-$string['day_ago'] = 'fa {$a} dia';
-$string['days_ago'] = 'fa {$a} dies';
-$string['month_ago'] = 'fa {$a} mes';
-$string['months_ago'] = 'fa {$a} mesos';
-$string['year_ago'] = 'fa {$a} any';
-$string['years_ago'] = 'fa {$a} anys';
-
 $string['agora_scripts'] = 'Àgora Scripts';
-
 $string['login_nodes'] = 'Entra a Nodes';
-$string['mygrades'] = 'Les meves qualificacions';
-
-$string['mailer'] = 'Mailer';
-$string['apligestmail'] = 'Activa el Mailer';
-$string['mailheader'] = 'Afegeix prefix als assumptes';
-$string['apligestlog'] = 'Activa el Log';
-$string['apligestlogdebug'] = 'Activa el Debug Log';
-$string['apligestlogpath'] = 'Camí de Log';
-$string['environment_url'] = 'WSDL URL';
-$string['environment_url_desc'] = 'Si està activada, es farà el WSDL per enviar correu, sinó, es farà servir la url per defecte per l\'entorn {$a}';
-
 $string['clean_adware'] = 'Neteja Adware';
 $string['detect_adware'] = 'Detecta Adware';
-$string['messagehistoryfull'] = 'Tots els missatges';
 
 // H5P
 $string['hvpdisabled'] = 'L\'activitat «HVP - Contingut interactiu» està inhabilitada. Utilitzeu l\'activitat «H5P» en el seu lloc.';
